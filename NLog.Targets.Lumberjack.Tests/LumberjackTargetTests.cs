e16afc7eaca1934a863803bde77ca7bb15fd7d55
Vasiliuk Sergey <vasiliuk@hotmail.com>
2015-07-30 12:41:58 +0300

    write test added

---
+++ NLog.Targets.Lumberjack.Tests/LumberjackTargetTests.cs
@@ -0,0 +1,43 @@
+﻿using NLog.Targets.Lumberjack;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace NLog.Targets.Lumberjack.Tests
+{
+    [TestClass]
+    public class LumberjackTargetTests
+    {
+        class LumberjackTargetAccessor : LumberjackTarget
+        {
+            public static Task WriteAsyncLogEventTest()
+            {
+                ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => { return true; };
+
+
+                var tcs = new TaskCompletionSource<object>();
+
+                var target = new LumberjackTargetAccessor()
+                {
+                    Host = "logs.zheka.by",
+                    Port = 5000,
+                    Fingerprint = "06E304EECEAEA7E3D0E8507B9012444503EF0053"
+                };
+
+                var evt = new Common.AsyncLogEventInfo(new LogEventInfo(LogLevel.Info, "Lumberjack", "WriteAsyncLogEventTest") { TimeStamp = DateTime.UtcNow }, e => { tcs.SetResult(null); });
+
+                target.Write(evt);
+
+                return tcs.Task;
+            }
+        
+        }
+
+        [TestMethod]
+        public void WriteAsyncLogEventTest()
+        {
+            Task.Run(LumberjackTargetAccessor.WriteAsyncLogEventTest).GetAwaiter().GetResult();
+        }
+    }
+}
--- NLog.Targets.Lumberjack.Tests/Properties/AssemblyInfo.cs
+++ NLog.Targets.Lumberjack.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NLog.Targets.Lumberjack")]
-[assembly: AssemblyDescription("A custom target for NLog for the Lumberjack protocol.")]
+[assembly: AssemblyTitle("NLog.Targets.Lumberjack.Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Joakim Beijar")]
-[assembly: AssemblyProduct("NLog.Targets.Lumberjack")]
-[assembly: AssemblyCopyright("Copyright © Joakim Beijar 2015")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NLog.Targets.Lumberjack.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("925b2b14-4be9-433b-be17-528960fee1ed")]
+[assembly: Guid("f81cfe78-a045-4d66-b325-71593f9db3d3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,7 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-
-
-[assembly: InternalsVisibleTo("NLog.Targets.Lumberjack.Tests")]
